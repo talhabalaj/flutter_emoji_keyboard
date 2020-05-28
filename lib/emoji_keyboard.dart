ef15bdd6f14eaca3778258f98e13cbf723e8e8ab
Talha Balaj <talhabalaj@hotmail.com>
2020-05-28 17:55:22 +0500

    initial commit

---
+++ lib/emoji_keyboard.dart
@@ -0,0 +1,17 @@
+library emoji_keyboard;
+
+import 'package:flutter/material.dart';
+
+class EmojiKeyBoard extends StatefulWidget {
+  EmojiKeyBoard({Key key}) : super(key: key);
+
+  @override
+  _EmojiKeyBoardState createState() => _EmojiKeyBoardState();
+}
+
+class _EmojiKeyBoardState extends State<EmojiKeyBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
